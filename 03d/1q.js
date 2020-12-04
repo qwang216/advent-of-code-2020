114f5e7b6ff58a54ef99fbcbf7910e2aa3600abd
Jason W <qwang216@gmail.com>
2020-12-04 15:26:08 -0500

    day3 (#2)
    * day3

--- 03d/1q.js
+++ 03d/1q.js
@@ -1,26 +1,29 @@
 const { getData } = require("../helper.js")
 
 function getResult(inputData) {
-  const grid = parseDataToMatrix(inputData)
-  let treeCount = 0
+    const grid = parseDataToMatrix(inputData)
+    if (grid.length < 1 || grid[0].length < 1) return 0
 
-  if (grid.length < 1 || grid[0].length < 1) return treeCount
-
-  let rowIndex = 0
-  let colIndex = 0
-  const deltaRow = 1
-  const deltaCol = 3
-
-  for (let i = 0; i < grid.length; i++) {
-    if (rowIndex > grid.length - 1 || colIndex > grid[0].length - 1) break // reset deltaCol?
-    rowIndex += deltaRow
-    colIndex += deltaCol
-    if (grid[rowIndex][colIndex] == "#") treeCount++
-  }
-  return treeCount
+    let treeCount = 0
+    let rowIndex = 0
+    let colIndex = 0
+    const deltaRow = 1
+    const deltaCol = 3
+    const col = grid[0].length
+    while (rowIndex < grid.length) {
+        if (grid[rowIndex][colIndex] === "#") treeCount++
+        colIndex = (colIndex + deltaCol) % col
+        rowIndex += deltaRow
+    }
+    return treeCount
 }
 
 function parseDataToMatrix(data) {
-  return //[[]]
+    var matrix = []
+    for (let i = 0; i < data.length; i++) {
+        let array = data[i].split("")
+        matrix.push(array)
+    }
+    return matrix
 }
 console.log(getResult(getData("input.txt")))
